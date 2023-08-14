ed495f7d892845d3f4909e34dc4e835e641da0ab
Radosław Drab <radoslaw.drab03@gmail.com>
2023-08-14 13:19:58 +0200

    configured app

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,31 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-  };
-};
+const config = require('./tsconfig.json')
+
+const { baseUrl, paths } = config.compilerOptions
+
+const getAliases = () => {
+	return Object.entries(paths).reduce((aliases, alias) => {
+		const key = alias[0].replace('/*', '')
+		const value = baseUrl + alias[1][0].replace('*', '')
+		return {
+			...aliases,
+			[key]: value
+		}
+	}, {})
+}
+
+module.exports = function (api) {
+	api.cache(true)
+	return {
+		presets: ['module:metro-react-native-babel-preset'],
+		plugins: [
+			[
+				'module-resolver',
+				{
+					root: ['./src'],
+					extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+					alias: getAliases()
+				}
+			]
+		]
+	}
+}
