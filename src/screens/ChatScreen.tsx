c56182d4a2137de00264b698af30c66c2c62b510
Radosław Drab <radoslaw.drab03@gmail.com>
2023-08-15 13:05:55 +0200

    added Chat and Rooms screens

---
+++ src/screens/ChatScreen.tsx
@@ -0,0 +1,10 @@
+import { Text } from 'react-native'
+
+const ChatScreen = (props: any) => {
+	return (
+		<>
+			<Text>Chats</Text>
+		</>
+	)
+}
+export default ChatScreen
---
+++ src/screens/RoomsScreen.tsx
@@ -0,0 +1,10 @@
+import { Text } from 'react-native'
+
+const RoomsScreen = () => {
+	return (
+		<>
+			<Text>Rooms</Text>
+		</>
+	)
+}
+export default RoomsScreen
