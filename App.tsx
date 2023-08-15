d6dadd7cf48873945b6f475891906cc6c9c48dd1
Radosław Drab <radoslaw.drab03@gmail.com>
2023-08-15 18:10:41 +0200

    added screens and font loading to App

--- App.tsx
+++ App.tsx
@@ -1,18 +1,55 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { ApolloProvider } from '@apollo/client'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import React, { useCallback } from 'react'
+import { View } from 'react-native'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
+import { useFonts } from 'expo-font'
+import * as SplashScreen from 'expo-splash-screen'
+
+import apolloClient from 'api/index'
+
+import { AppStackParamList } from './types'
+
+import ChatScreen from 'screens/ChatScreen'
+import RoomsScreen from 'screens/RoomsScreen'
+
+import Header from 'components/Header/Header'
+
+const fonts = {
+	PoppinsMedium: require('./assets/fonts/Poppins-Medium.ttf'),
+	PoppinsRegular: require('./assets/fonts/Poppins-Regular.ttf'),
+	PoppinsSemiBold: require('./assets/fonts/Poppins-SemiBold.ttf'),
+	PoppinsBold: require('./assets/fonts/Poppins-Bold.ttf')
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Stack = createNativeStackNavigator<AppStackParamList>()
+
+SplashScreen.preventAutoHideAsync()
+
+export default function App() {
+	const [fontsLoaded, fontError] = useFonts(fonts)
+
+	const onLayoutRootView = useCallback(async () => {
+		if (fontsLoaded || fontError) {
+			await SplashScreen.hideAsync()
+		}
+	}, [fontsLoaded, fontError])
+
+	if (!fontsLoaded && !fontError) {
+		return null
+	}
+
+	return (
+		<ApolloProvider client={apolloClient}>
+			<NavigationContainer>
+				<View onLayout={onLayoutRootView}>
+					<Stack.Navigator initialRouteName="Rooms" screenOptions={{ header: Header }}>
+						<Stack.Screen name="Rooms" component={RoomsScreen} />
+						<Stack.Screen name="Chat" component={ChatScreen} />
+					</Stack.Navigator>
+				</View>
+			</NavigationContainer>
+		</ApolloProvider>
+	)
+}
