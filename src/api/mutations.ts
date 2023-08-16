fabb3c0d7db5cc433b79e337b9919665eb544bc9
Radosław Drab <radoslaw.drab03@gmail.com>
2023-08-16 17:34:56 +0200

    added GraphQL mutations

---
+++ src/api/mutations.ts
@@ -0,0 +1,18 @@
+import { gql } from '@apollo/client'
+
+export const SEND_MESSAGE = gql`
+	mutation SendMessage($body: String!, $roomId: String!) {
+		sendMessage(body: $body, roomId: $roomId) {
+			body
+			id
+			insertedAt
+			user {
+				id
+				firstName
+				lastName
+				role
+				email
+			}
+		}
+	}
+`
