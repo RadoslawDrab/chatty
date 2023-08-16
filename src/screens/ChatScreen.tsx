bf3a8c9608e1ad69bc86d611b81ece3ec3ad1393
Radosław Drab <radoslaw.drab03@gmail.com>
2023-08-16 14:58:29 +0200

    added screen backgrounds

--- src/screens/ChatScreen.tsx
+++ src/screens/ChatScreen.tsx
@@ -8,6 +8,8 @@ import { renderBubble, renderInputToolbar, renderSend } from './Chat/renders'
 
 import { AppStackParamList } from '../types.modal'
 
+import { PageBackground } from 'styles/generic'
+
 import lib from 'styles/library'
 
 const ChatScreen = (props: NativeStackScreenProps<AppStackParamList, 'Chat'>) => {
@@ -41,22 +43,27 @@ const ChatScreen = (props: NativeStackScreenProps<AppStackParamList, 'Chat'>) =>
 	}, [])
 
 	return (
-		<SafeAreaView style={{ flex: 1 }}>
-			<GiftedChat
-				messages={messages}
-				onSend={(messages: []) => onSend(messages)}
-				user={{
-					_id: 1
-				}}
-				alwaysShowSend
-				renderTime={() => <></>}
-				renderSend={renderSend}
-				renderBubble={renderBubble}
-				forceGetKeyboardHeight
-				renderInputToolbar={renderInputToolbar}
-				messagesContainerStyle={{ paddingBottom: lib.sizing.medium * 4, marginBottom: -32 }}
-			/>
-		</SafeAreaView>
+		<PageBackground>
+			<SafeAreaView style={{ flex: 1 }}>
+				<GiftedChat
+					messages={messages}
+					onSend={(messages: []) => onSend(messages)}
+					user={{
+						_id: 1
+					}}
+					alwaysShowSend
+					renderTime={() => <></>}
+					renderSend={renderSend}
+					renderBubble={renderBubble}
+					forceGetKeyboardHeight
+					renderInputToolbar={renderInputToolbar}
+					messagesContainerStyle={{
+						paddingBottom: lib.sizing.medium * 4,
+						marginBottom: -32
+					}}
+				/>
+			</SafeAreaView>
+		</PageBackground>
 	)
 }
 export default ChatScreen
